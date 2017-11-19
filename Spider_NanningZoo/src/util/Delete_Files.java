f7af66c636bfe9be97a353988a06c73893137e82
hhqiwei <hhqiwei@192.168.1.102>
2017-11-19 09:54:50 +0800

    Firtst Version2017-11-19 星期日 9:53:49
    Change-Id: If4bf8107f90673f77108c20f5447f9536ea2603d
    Signed-off-by: hhqiwei <hhqiwei@192.168.1.102>

---
+++ Spider_NanningZoo/src/util/Delete_Files.java
@@ -0,0 +1,56 @@
+package util;
+
+import java.io.File;
+
+/**
+ * @ClassName: Delete_Files
+ * @Description: 删除temp_html下的文件
+ * @author 黄启威
+ * @date 2017年11月18日 下午5:29:33
+ *
+ */
+public class Delete_Files {
+	public static void delFolder(String folderPath) {
+		try {
+			delAllFile(folderPath); // 删除完里面所有内容
+			String filePath = folderPath;
+			filePath = filePath.toString();
+			// java.io.File myFilePath = new java.io.File(filePath);
+			// myFilePath.delete(); // 删除空文件夹
+			System.out.println("成功删除文件夹" + filePath + "中的文件！");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// 删除指定文件夹下所有文件
+	// param path 文件夹完整绝对路径
+	public static boolean delAllFile(String path) {
+		boolean flag = false;
+		File file = new File(path);
+		if (!file.exists()) {
+			return flag;
+		}
+		if (!file.isDirectory()) {
+			return flag;
+		}
+		String[] tempList = file.list();
+		File temp = null;
+		for (int i = 0; i < tempList.length; i++) {
+			if (path.endsWith(File.separator)) {
+				temp = new File(path + tempList[i]);
+			} else {
+				temp = new File(path + File.separator + tempList[i]);
+			}
+			if (temp.isFile()) {
+				temp.delete();
+			}
+			if (temp.isDirectory()) {
+				delAllFile(path + "/" + tempList[i]);// 先删除文件夹里面的文件
+				delFolder(path + "/" + tempList[i]);// 再删除空文件夹
+				flag = true;
+			}
+		}
+		return flag;
+	}
+}
